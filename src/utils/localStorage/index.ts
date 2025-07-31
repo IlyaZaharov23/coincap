db81a603ee71e1892a09fe4c283749f2cc3936b9
Elistar_DJ <elistardj@outlook.com>
2025-07-31 23:51:04 +0300

    remove legacy

--- src/utils/localStorage/index.ts
+++ src/utils/localStorage/index.ts
@@ -2,30 +2,6 @@ import { COINS_PORTFOLIO } from "services/constants";
 import { WalletItem } from "store/slices/assets/assets.types";
 
 export class LocalStorageUtil {
-    static addCoinsToLocalStorage = (coinInfo: WalletItem, coinId: string) => {
-        const localWallet = localStorage.getItem(COINS_PORTFOLIO);
-        if (localWallet) {
-            const parsedWallet = JSON.parse(localWallet);
-            const toLocalStorage: { [coinId: string]: WalletItem } = parsedWallet;
-            toLocalStorage[coinId] = coinInfo;
-            localStorage.setItem(COINS_PORTFOLIO, JSON.stringify(toLocalStorage));
-        } else {
-            const toLocalStorage: { [coinId: string]: WalletItem } = {};
-            toLocalStorage[coinId] = coinInfo;
-            localStorage.setItem(COINS_PORTFOLIO, JSON.stringify(toLocalStorage));
-        }
-    };
-
-    static removeCoinsFromLocalStorage = (coinId: string) => {
-        const localWallet = localStorage.getItem(COINS_PORTFOLIO);
-        if (localWallet) {
-            const parsedWallet = JSON.parse(localWallet);
-            const toLocalStorage: { [coinId: string]: WalletItem } = parsedWallet;
-            delete toLocalStorage[coinId];
-            localStorage.setItem(COINS_PORTFOLIO, JSON.stringify(toLocalStorage));
-        }
-    };
-
     static getUserPortfolio(userId: string) {
         const portfolios = localStorage.getItem(COINS_PORTFOLIO);
         if (!portfolios) return null;
@@ -54,14 +30,6 @@ export class LocalStorageUtil {
         if (Object.keys(parsedPortfolios[userId].length === 0)) {
             delete parsedPortfolios[userId];
         }
-        localStorage.setItem(COINS_PORTFOLIO, JSON.stringify(parsedPortfolios));
-    }
-
-    static clearUserPortfolio(userId: string) {
-        const portfolios = localStorage.getItem(COINS_PORTFOLIO);
-        if (!portfolios) return;
-        const parsedPortfolios = JSON.parse(portfolios);
-        delete parsedPortfolios[userId];
         localStorage.setItem(COINS_PORTFOLIO, JSON.stringify(parsedPortfolios));
     }
 }
