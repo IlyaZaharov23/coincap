b16361edc75a1031be8dd744cae1cc385f741b37
Elistar_DJ <elistardj@outlook.com>
2026-03-16 22:30:01 +0300

    fix mobile and tablet coin styles

--- src/components/TabletCoin/view.tsx
+++ src/components/TabletCoin/view.tsx
@@ -5,11 +5,16 @@ import { memo } from "react";
 import { Stack, Text } from "@chakra-ui/react";
 
 import { CryptoIcon } from "components/CryptoIcon";
+import { Divider } from "components/Divider";
 import { useRouter } from "next/navigation";
+import { black } from "shared/constants/colors";
+import { DIVIDER_ORIENTATION, DIVIDER_SIZE } from "shared/constants/divider";
 import { ROUTES } from "shared/constants/routes";
 import { useAppDispatch } from "store/hooks";
 import { setAssetDetails } from "store/slices/assets/actions";
+import { getPriceArrowIcon } from "utils/helpers/price";
 import { PricesUtil } from "utils/prices";
+import { StyleUtil } from "utils/style";
 import { TextUtil } from "utils/text";
 
 import { styles } from "./styles";
@@ -26,10 +31,29 @@ export const TabletCoin = memo(({ asset }: TabletCoinProps) => {
     return (
         <Stack sx={styles.mainWrapper} onClick={() => navigateToCoin(asset.id)}>
             <Stack sx={styles.coinWrapper}>
-                <CryptoIcon symbol={asset.symbol} size={44} />
                 <Stack sx={styles.nameWrapper}>
-                    <Text sx={styles.name}>{asset.name}</Text>
+                    <CryptoIcon symbol={asset.symbol} size={44} />
+                    <Stack sx={styles.priceWrapper}>
+                        <Text sx={styles.name}>{asset.name}</Text>
+                        <Text sx={styles.price}>{PricesUtil.formatAsCurrency(asset.priceUsd)}</Text>
+                    </Stack>
+                </Stack>
+                <Divider orientation={DIVIDER_ORIENTATION.VERTICAL} color={black} size={DIVIDER_SIZE.SMALL} />
+                <Stack sx={styles.priceWrapper}>
                     <Text sx={styles.price}>{PricesUtil.formatAsCurrency(asset.priceUsd)}</Text>
+                    <Stack sx={styles.priceCountWrapper}>
+                        <Text
+                            sx={{
+                                ...styles.price,
+                                color: StyleUtil.getCurrencyPriceChangeColor(
+                                    PricesUtil.getPriceStatus(asset.vwap24Hr, asset.changePercent24Hr),
+                                ),
+                            }}
+                        >
+                            {PricesUtil.getVWAPChangeValue(asset.vwap24Hr, asset.changePercent24Hr) || 0}
+                        </Text>
+                        {getPriceArrowIcon(asset.vwap24Hr, asset.changePercent24Hr)}
+                    </Stack>
                 </Stack>
             </Stack>
         </Stack>
